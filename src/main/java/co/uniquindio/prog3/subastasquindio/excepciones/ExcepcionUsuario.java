959863e78da17ac63dd7eb74f629e0aefc288d03
miguelvargas2110 <miguela.vargasn@uqvirtual.edu.co>
2022-10-08 15:04:22 -0500

    Excepciones y Interface creadas

--- src/main/java/co/uniquindio/prog3/subastasquindio/excepciones/ExcepcionUsuario.java
+++ src/main/java/co/uniquindio/prog3/subastasquindio/excepciones/ExcepcionUsuario.java
@@ -5,4 +5,6 @@ public class ExcepcionUsuario extends Exception {
         super(mensaje);
     }
 
+    public ExcepcionUsuario() {
+    }
 }
---
+++ src/main/java/co/uniquindio/prog3/subastasquindio/modelo/OperacionesCrud.java
@@ -0,0 +1,8 @@
+package co.uniquindio.prog3.subastasquindio.modelo;
+
+public interface OperacionesCrud {
+    public void guardarEnCrud();
+    public void editarEnCrud();
+    public void eliminarEnCrud();
+    public void crearEnCrud();
+}
